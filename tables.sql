69cda67a30c77b85096116142cf6f09b3ba15d57
elsieeskandar <elsie.eskandar@gmail.com>
2018-03-11 19:29:59 -0400

    adjusted bb specs foreign keys

--- tables.sql
+++ tables.sql
@@ -1,21 +1,23 @@
+DROP TABLE IF EXISTS bb_specs;
 DROP TABLE IF EXISTS shape_molds;
 DROP TABLE IF EXISTS colour_scheme;
 DROP TABLE IF EXISTS essential_oils_stock;
 DROP TABLE IF EXISTS employee_login;
+DROP TABLE IF EXISTS products_selected;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS purchase_order;
+DROP TABLE IF EXISTS customer_skin_shade;
+DROP TABLE IF EXISTS customer_info;
 DROP TABLE IF EXISTS credit_card_info;
 DROP TABLE IF EXISTS address;
-DROP TABLE IF EXISTS customer_info;
-DROP TABLE IF EXISTS products_selected;
-DROP TABLE IF EXISTS bb_specs;
-DROP TABLE IF EXISTS purchase_order;
-DROP TABLE IF EXISTS shade_range_group;
 DROP TABLE IF EXISTS skin_shade_match;
-DROP TABLE IF EXISTS customer_skin_shade;
+DROP TABLE IF EXISTS shade_range_group;
+
 
 CREATE TABLE shape_molds (
-shape VARCHAR (50) PRIMARY KEY,
-size VARCHAR (50) PRIMARY KEY);
+shape VARCHAR (50) NOT NULL,
+bb_size VARCHAR (50) NOT NULL,
+PRIMARY KEY (shape, bb_size));
 
 CREATE TABLE colour_scheme (
 colour_scheme_name VARCHAR (50) PRIMARY KEY,
@@ -32,13 +34,13 @@ password VARCHAR (256) NOT NULL ); -- password has a large field size for encryp
 CREATE TABLE products (
 product_id VARCHAR (20) PRIMARY KEY,
 product_category VARCHAR (50) NOT NULL,
-product_name VARCHAR (50 NOT NULL
+product_name VARCHAR (50) NOT NULL);
 
 CREATE TABLE credit_card_info (
 cardnum VARCHAR (20) PRIMARY KEY,
 cardname VARCHAR (50) NOT NULL,
 cvc VARCHAR (256) NOT NULL, -- cvc has a large field size for encryption using sha2
-expiry_date DATE) NOT NULL;
+expiry_date DATE NOT NULL);
 
 CREATE TABLE address (
 address_id VARCHAR (20) PRIMARY KEY,
@@ -59,25 +61,27 @@ FOREIGN KEY (cardnum) REFERENCES credit_card_info (cardnum),
 FOREIGN KEY (address_id) REFERENCES address (address_id));
 
 CREATE TABLE products_selected (
-cust_id VARCHAR (20) PRIMARY KEY,
-product_id VARCHAR (20) PRIMARY KEY,
+cust_id VARCHAR (20),
+product_id VARCHAR (20),
 quantity INT NOT NULL,
 FOREIGN KEY (cust_id) REFERENCES customer_info (cust_id),
-FOREIGN KEY (product_id) REFERENCES products (product_id) 
+FOREIGN KEY (product_id) REFERENCES products (product_id),
+PRIMARY KEY (cust_id, product_id) 
 );
 
 CREATE TABLE bb_specs (
-bb_num VARCHAR (20) PRIMARY KEY,
-cust_id VARCHAR (20) PRIMARY KEY,
+bb_num VARCHAR (20),
+cust_id VARCHAR (20),
 shape VARCHAR (50) NOT NULL,
-size VARCHAR (50) NOT NULL,
+bb_size VARCHAR (50) NOT NULL,
 colour_scheme_name VARCHAR (50) NOT NULL,
 essential_oil VARCHAR (50) NOT NULL,
 FOREIGN KEY (cust_id) REFERENCES customer_info (cust_id),
-FOREIGN KEY (shape) REFERENCES shape_molds (shape)
-FOREIGN KEY (size) REFERENCES shape_molds (size)
+-- FOREIGN KEY (bb_size) REFERENCES shape_molds (bb_size),
+FOREIGN KEY (shape) REFERENCES shape_molds (shape),
 FOREIGN KEY (colour_scheme_name) REFERENCES colour_scheme (colour_scheme_name),
-FOREIGN KEY (essential_oil) REFERENCES essential_oils_stock (essential_oil));
+FOREIGN KEY (essential_oil) REFERENCES essential_oils_stock (essential_oil_name),
+PRIMARY KEY (bb_num, cust_id));
 
 CREATE TABLE purchase_order (
 cust_id VARCHAR(20) PRIMARY KEY,
@@ -95,7 +99,8 @@ lush_shade_name VARCHAR (50) NOT NULL,
 FOREIGN KEY (lush_shade_name) REFERENCES shade_range_group(lush_shade_name));
 
 CREATE TABLE customer_skin_shade(
-skin_shade_hex INT PRIMARY KEY,
-cust_id VARCHAR (20) PRIMARY KEY,
+skin_shade_hex INT,
+cust_id VARCHAR (20),
 FOREIGN KEY (skin_shade_hex) REFERENCES skin_shade_match(skin_shade_hex),
-FOREIGN KEY (cust_id) REFERENCES customer_info(cust_id));
+FOREIGN KEY (cust_id) REFERENCES customer_info(cust_id),
+PRIMARY KEY (skin_shade_hex, cust_id));
